57d16bad4d64e478e69d8381ec1d0dcb2c47bdf7
yelyzaveta posheliuk <nemaeimya@gmail.com>
2025-02-06 22:39:27 +0200

    fixes

--- backend/src/main/python/app.py
+++ backend/src/main/python/app.py
@@ -1,10 +1,10 @@
-from Flask import Flask, request, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/echo')
+@app.route('/echo', methods = ['GET', 'POST'])
 def doecho():
     return jsonify(request.json)
 
 if __name__ == "__main__":
-    app.run(dubug=True, host='127.0.0.1',port=8888)
+    app.run(debug=True, host='127.0.0.1',port=6060)

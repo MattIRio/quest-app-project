d158be052f0bed427cde47a26dae686b3a795b71
Devomel <omellwork@gmail.com>
2025-02-10 12:47:35 +0200

    router with solved conficts

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,23 +1,17 @@
 import Auth from "../components/Auth/AuthForm";
 import Main from "../pages/Main";
 import CreateQuest from "../pages/CreateQuest.jsx";
-<<<<<<< HEAD
 import QuestCompletion from "../pages/QuestCompletion.jsx";
-=======
 import Profile from "../pages/Profile.jsx";
->>>>>>> 64dd24c0b29b046f5ea932f27b69479ddc566d41
 
 
 export const publicRoutes = [
    { path: "/auth", component: Auth },
    { path: "/main", component: Main },
    { path: "/create_quest", component: CreateQuest },
-<<<<<<< HEAD
-   { path: "/quests/:questId", component: QuestCompletion }
-=======
-   {path: "/profile", component: Profile },
+   { path: "/quests/:questId", component: QuestCompletion },
+   { path: "/profile", component: Profile },
 
->>>>>>> 64dd24c0b29b046f5ea932f27b69479ddc566d41
 ]
 
 export const privateRoutes = [
